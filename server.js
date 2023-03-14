c11c374fd7229dcc2152b6a14f1103de2b48fa05
Umar Farooq <umarfarooq9@carecloud.com>
2023-03-14 17:21:55 +0500

    call python app in nodejsapp

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 'use strict';
 
 const express = require('express');
-
+const axios = require("axios");
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
+const PORT = 8083;
+const HOST = 'localhost';
+const config = require('config');
+const callport = config.get('server.port');
+const callhost = config.get('server.host');
+const baseUrl = "http://"+callhost+":"+callport+"/"
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/call-python-app',async (req, res) => {
+  try {
+		const response = await axios({
+      headers: { Accept: 'text/html, application/json, text/plain, */*' },
+     proxy: false,
+
+			url: baseUrl,
+			method: "get",
+		});
+		res.status(200).json(response.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+})
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
